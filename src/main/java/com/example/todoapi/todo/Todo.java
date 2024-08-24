6e1aa9b1d19eb85e706098ebe64de2061ca40772
kckc0608 <kckc0608@gmail.com>
2024-08-24 15:07:18 +0900

    feat: todo, user entity

---
+++ src/main/java/com/example/todoapi/todo/Todo.java
@@ -0,0 +1,32 @@
+package com.example.todoapi.todo;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import com.example.todoapi.user.User;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_id")
+    private Long id;
+
+    @Column(name = "todo_content", columnDefinition = "varchar(200)")
+    private String content;
+
+    @Column(name = "todo_is_check", columnDefinition = "tinyint(1)")
+    private boolean isChecked;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
