aac9c041fd50579d7e5434b04ce2da95cf79d832
kckc0608 <kckc0608@naver.com>
2024-10-04 00:44:45 +0900

    week4 init

--- src/main/java/com/example/todoapi/todo/Todo.java
+++ src/main/java/com/example/todoapi/todo/Todo.java
@@ -32,7 +32,7 @@ public class Todo {
     private boolean isChecked;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "member_id")
     private Member member;
 
     public Todo(String content, boolean isChecked, Member member) {
