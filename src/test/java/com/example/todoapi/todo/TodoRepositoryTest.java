c2f447eb3f2415704d5e63c4c51b8e9fc264bd3c
kckc0608 <kckc0608@naver.com>
2024-11-20 12:02:02 +0900

    test: 테스트 코드 수정

--- src/test/java/com/example/todoapi/todo/TodoRepositoryTest.java
+++ src/test/java/com/example/todoapi/todo/TodoRepositoryTest.java
@@ -21,104 +21,104 @@ public class TodoRepositoryTest {
     @Autowired
     private MemberRepository memberRepository;
 
-    @Test
-    @Transactional
-    @Rollback(false)
-    void todoSaveTest() {
-        // 트랜잭션의 시작
-        Todo todo = new Todo("todo content", null);
-        todoRepository.save(todo);
-
-        // 트랜잭션 종료 => 커밋
-        // 에러가 발생했을 때는 자동으로 롤백
-
-        Assertions.assertThat(todo.getId()).isNotNull();
-        // 테스트 환경 기준으로는, 에러가 발생하지 않아도, 테스트가 끝나면 자동으로 롤백. => 수동으로 롤백을 꺼주면 됩니다.
-    }
-
-    @Test
-    @Transactional
-    void todoFindOneByIdTest() {
-        // given
-        Todo todo = new Todo("todo content", null);
-        todoRepository.save(todo);
-
-        todoRepository.flushAndClear();
-
-        // when
-        Todo findTodo = todoRepository.findById(todo.getId());
-
-        // then
-        Assertions.assertThat(findTodo.getId()).isEqualTo(todo.getId());
-    }
-
-    @Test
-    @Transactional
-    void todoFindAllTest() {
-        Todo todo1 = new Todo("todo content1", null);
-        Todo todo2 = new Todo("todo content2", null);
-        Todo todo3 = new Todo("todo content3", null);
-        todoRepository.save(todo1);
-        todoRepository.save(todo2);
-        todoRepository.save(todo3);
-
-        List<Todo> todoList = todoRepository.findAll();
-
-        Assertions.assertThat(todoList).hasSize(3);
-    }
-
-    @Test
-    @Transactional
-    void todoFindAllByMemberTest() {
-        Member member1 = new Member();
-        Member member2 = new Member();
-        memberRepository.save(member1);
-        memberRepository.save(member2);
-
-        Todo todo1 = new Todo("todo content1", member1);
-        Todo todo2 = new Todo("todo content2", member1);
-        Todo todo3 = new Todo("todo content3", member2);
-        todoRepository.save(todo1);
-        todoRepository.save(todo2);
-        todoRepository.save(todo3);
-
-        List<Todo> member1TodoList = todoRepository.findAllByMember(member1);
-        List<Todo> member2TodoList = todoRepository.findAllByMember(member2);
-
-        Assertions.assertThat(member1TodoList).hasSize(2);
-        Assertions.assertThat(member2TodoList).hasSize(1);
-    }
-
-    @Test
-    @Transactional
-    @Rollback(false)
-    void todoUpdateTest() {
-        Todo todo1 = new Todo("todo content1", null);
-        todoRepository.save(todo1);
-
-        todoRepository.flushAndClear();
-
-        Todo findTodo1 = todoRepository.findById(todo1.getId());
-        findTodo1.updateContent("new Content");
-    }
-
-    @Test
-    @Transactional
-    @Rollback(false)
-    void todoDeleteTest() {
-        Todo todo1 = new Todo("todo content1", null);
-        Todo todo2 = new Todo("todo content2", null);
-        todoRepository.save(todo1);
-        todoRepository.save(todo2);
-
-        todoRepository.flushAndClear();
-
-        todoRepository.deleteById(todo1.getId());
-    }
-
-    @AfterAll
-    public static void doNotFinish() {
-        System.out.println("test finished");
-        while (true) {}
-    }
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    void todoSaveTest() {
+//        // 트랜잭션의 시작
+//        Todo todo = new Todo("todo content", null);
+//        todoRepository.save(todo);
+//
+//        // 트랜잭션 종료 => 커밋
+//        // 에러가 발생했을 때는 자동으로 롤백
+//
+//        Assertions.assertThat(todo.getId()).isNotNull();
+//        // 테스트 환경 기준으로는, 에러가 발생하지 않아도, 테스트가 끝나면 자동으로 롤백. => 수동으로 롤백을 꺼주면 됩니다.
+//    }
+//
+//    @Test
+//    @Transactional
+//    void todoFindOneByIdTest() {
+//        // given
+//        Todo todo = new Todo("todo content", null);
+//        todoRepository.save(todo);
+//
+//        todoRepository.flushAndClear();
+//
+//        // when
+//        Todo findTodo = todoRepository.findById(todo.getId());
+//
+//        // then
+//        Assertions.assertThat(findTodo.getId()).isEqualTo(todo.getId());
+//    }
+//
+//    @Test
+//    @Transactional
+//    void todoFindAllTest() {
+//        Todo todo1 = new Todo("todo content1", null);
+//        Todo todo2 = new Todo("todo content2", null);
+//        Todo todo3 = new Todo("todo content3", null);
+//        todoRepository.save(todo1);
+//        todoRepository.save(todo2);
+//        todoRepository.save(todo3);
+//
+//        List<Todo> todoList = todoRepository.findAll();
+//
+//        Assertions.assertThat(todoList).hasSize(3);
+//    }
+//
+//    @Test
+//    @Transactional
+//    void todoFindAllByMemberTest() {
+//        Member member1 = new Member();
+//        Member member2 = new Member();
+//        memberRepository.save(member1);
+//        memberRepository.save(member2);
+//
+//        Todo todo1 = new Todo("todo content1", member1);
+//        Todo todo2 = new Todo("todo content2", member1);
+//        Todo todo3 = new Todo("todo content3", member2);
+//        todoRepository.save(todo1);
+//        todoRepository.save(todo2);
+//        todoRepository.save(todo3);
+//
+//        List<Todo> member1TodoList = todoRepository.findAllByMember(member1);
+//        List<Todo> member2TodoList = todoRepository.findAllByMember(member2);
+//
+//        Assertions.assertThat(member1TodoList).hasSize(2);
+//        Assertions.assertThat(member2TodoList).hasSize(1);
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    void todoUpdateTest() {
+//        Todo todo1 = new Todo("todo content1", null);
+//        todoRepository.save(todo1);
+//
+//        todoRepository.flushAndClear();
+//
+//        Todo findTodo1 = todoRepository.findById(todo1.getId());
+//        findTodo1.updateContent("new Content");
+//    }
+//
+//    @Test
+//    @Transactional
+//    @Rollback(false)
+//    void todoDeleteTest() {
+//        Todo todo1 = new Todo("todo content1", null);
+//        Todo todo2 = new Todo("todo content2", null);
+//        todoRepository.save(todo1);
+//        todoRepository.save(todo2);
+//
+//        todoRepository.flushAndClear();
+//
+//        todoRepository.deleteById(todo1.getId());
+//    }
+//
+//    @AfterAll
+//    public static void doNotFinish() {
+//        System.out.println("test finished");
+//        while (true) {}
+//    }
 }
